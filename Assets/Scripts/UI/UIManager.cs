f84cf64f63abf279d6e1185d5e14764f235b46e9
Jorge R. Hill <jorgerousseau80@gmail.com>
2025-07-01 13:30:53 -0400

    Subtle Commit

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UIElements;
 
-public class StartUI : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     UIDocument DOM;
     private VisualElement UpperCoroutine;
