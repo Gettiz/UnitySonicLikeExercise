a7036e67ef3d3966bb4eed493f898a7e3ab0c0b5
Jorge R. Hill <jorgerousseau80@gmail.com>
2025-07-02 15:35:52 -0400

    Reference problem UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -2,50 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
-    
+
     private VisualElement UxmlStart;
     private VisualElement UxmlGame;
     private VisualElement UxmlLoading;
     private VisualElement UxmlPause;
-    
+
     [SerializeField] private UIDocument DOM;
-    
+
     [SerializeField] private AudioSource playerAudio;
 
+    private string sceneName;
+    
     [SerializeField] private TemplateUIStart TUIStart;
     [SerializeField] private TemplateUIInGame TUIInGame;
-    
+    [SerializeField] private TemplateUIInGame TUIPause;
+
     public void Awake()
     {
+        DontDestroyOnLoad();
+
         VisualElement root = DOM.rootVisualElement;
         UxmlStart = root.Q<VisualElement>("TStartUI");
         UxmlGame = root.Q<VisualElement>("TGameUI");
-        UxmlLoading = root.Q<VisualElement>("TLoadingUI");
+        //UxmlLoading = root.Q<VisualElement>("TLoadingUI");
         UxmlPause = root.Q<VisualElement>("TPauseUI");
-        
-        DontDestroyOnLoad();
 
         TUIStart.Config(DOM, playerAudio);
         TUIInGame.Config(DOM, playerAudio);
+        TUIPause.Config(DOM, playerAudio);
+
+        UxmlStart.style.display = DisplayStyle.None;
+        UxmlGame.style.display = DisplayStyle.None;
+        UxmlPause.style.display = DisplayStyle.None;
+
+        SceneManager.GetSceneByName(sceneName);
+        switch (sceneName)
+        {
+            case "Scenes/Menu":
+                UxmlStart.style.display = DisplayStyle.Flex;
+                break;
+            case "Scenes/Game":
+                UxmlGame.style.display = DisplayStyle.Flex;
+                break;
+        }
     }
+
     private void Start()
     {
         TUIStart.TInitStart();
         TUIInGame.TInitInStart();
+        TUIPause.TInitInStart();
 
         TUIStart.CloseTemplateStart.AddListener(StartDisableStartUI);
     }
+
     public void StartDisableStartUI()
     {
         UxmlStart.style.display = DisplayStyle.None;
     }
 
-    
+
     private void DontDestroyOnLoad()
     {
         if (Instance != null)
