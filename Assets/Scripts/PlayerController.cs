b81a6bd8281270a9fdc8cf31fe5ce13313fc47e2
Jorge R. Hill <jorgerousseau80@gmail.com>
2025-07-06 03:50:40 -0400

    Movement And Cinemachine Camera added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using DefaultNamespace;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -10,9 +11,32 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameObject persistentUI;
     public UnityEvent TUIInGame;
-
-    public PlayerInput playerInput;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    //Object Properties
+    Rigidbody rb;
+    
+    //Move
+    private PlayerInput playerInput;
+    private Vector2 moveInput;
+    
+    public float maxSpeed = 10f;
+    
+    //Camera
+    private Vector2 cameraInput;
+    public CinemachineCamera CineCamera;
+    public CinemachineOrbitalFollow OrbitalCamera;
+    private InputAxis horizontalAxis;
+    private InputAxis verticalAxis;
+    
+    public float sensX;
+    public float sensY;
+    
+    //Object Atlas
+    public Transform orientation;
+    
+    float xRotation;
+    float yRotation;
+    
     private void Awake()
     {
         if (UIManager.Instance == null)
@@ -21,16 +45,39 @@ public class PlayerController : MonoBehaviour
         }
         UIManager.Instance.TUIInGame.SetGameStartedFlag(true);
     }
-
+    
     void Start()
     {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         
     }
     
+    void Update()
+    {
+        //Camera
+        horizontalAxis.Value += cameraInput.x;
+        verticalAxis.Value += cameraInput.y;
+
+        OrbitalCamera.HorizontalAxis = horizontalAxis;
+        OrbitalCamera.VerticalAxis = verticalAxis;
+        
+        //Move
+        moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+        cameraInput = playerInput.actions["Camera"].ReadValue<Vector2>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Move
+        Vector2 normalizedMoveInput = moveInput.normalized;
+        
+        rb.AddForce(new Vector3(normalizedMoveInput.x, 0f, normalizedMoveInput.y) * (maxSpeed * Time.fixedDeltaTime));
+        
+        
+    }
 }
